b92d418da57ba5dfa8cceda71655752a3e4cd330
Seng Chan You <applegna@gmail.com>
2018-03-22 13:26:55 +0900

    neurohacking wk1
    Signed-off-by: Seng Chan You <applegna@gmail.com>

---
+++ neurohacking_wk1.r
@@ -0,0 +1,15 @@
+##########
+##WEEK1
+##########
+
+devtools::install_github("muschellij2/fslr")
+devtools::install_github("stnava/ITKR")
+devtools::install_github("stnava/ANTsR")
+devtools::install_github("muschellij2/extrantsr")
+
+##acquiring data from
+#https://github.com/muschellij2/Neurohacking_data
+
+##########
+##########
+##########
